5e9c4e0ca67fbc0818b232f8b0269bab4486f792
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-10-06 21:57:30 -0600

    Html base and style complete.  logging API well

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-var apiKey = "59265ab185cc1f72e9ec2b2cdaa55519";
-$(function() {
-  console.log("jQuery works")
+$(function () {
+  var apiKey = "59265ab185cc1f72e9ec2b2cdaa55519";
+  var city = "Las+Vegas";
+  var lat = "36.17";
+  var lon = "-115.14";
+  var queryURL1 =  "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+  var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,minute,hourly,alerts&appid=" + apiKey;
+  $.ajax({
+    url: queryURL1,
+    method: "GET"
+  }).then(function (current) {
+    console.log(current);
+    $.ajax({
+      url: queryURL2,
+      method: "GET"
+    }).then(function(multiDay){
+      console.log(multiDay);
+    });
+  });
 });
